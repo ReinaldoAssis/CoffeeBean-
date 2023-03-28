2f439afea94a2dc033b977ba1e3c2b795ad09b57
tenorioalb <otalb@ic.ufal.br>
2023-03-28 18:52:12 -0300

    fluxo att

--- telas/FluxoCaixa.java
+++ telas/FluxoCaixa.java
@@ -4,23 +4,26 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import service.Controlador;
 import javafx.fxml.Initializable;
 
 
 public class FluxoCaixa implements Initializable {
 
     @FXML
-    private TableView<?> table_all;
+    private Label infosLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
         Controlador ctrl = Controlador.getInstance();
-        System.out.println("vendas :" + ctrl.database.venda + "\n" + "compras :" + ctrl.database.compra);
-        System.out.println("situação de caixa :" + (ctrl.database.compra-ctrl.database.venda));
+        infosLabel.setText("Compra total:" + ctrl.database.compra + "\n" + "Vendas total:" + ctrl.database.venda + "\n" + "Caixa:" + (ctrl.database.venda-ctrl.database.compra));
+        //System.out.println("vendas :" + ctrl.database.venda + "\n" + "compras :" + ctrl.database.compra);
+        //System.out.println("situação de caixa :" + (ctrl.database.compra-ctrl.database.venda));
      
         
     }
