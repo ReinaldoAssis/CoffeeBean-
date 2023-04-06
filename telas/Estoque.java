de968b91b22bf6089cb5093415f9296731c892e2
Reinaldo Assis <randroi75@gmail.com>
2023-04-06 14:51:28 -0400

    fix: reverti alterações que foram sobescritas pelo último push

--- telas/Estoque.java
+++ telas/Estoque.java
@@ -106,6 +106,8 @@ public class Estoque implements Initializable{
         boolean resultado = ctrl.database.removerProduto(ctxt, false);
         if(!resultado) JOptionPane.showMessageDialog(null, "Operação cancelada!");
         CarregarLista();
+        info.setText("");
+        produto.setText("Produto");
     }
    
 
