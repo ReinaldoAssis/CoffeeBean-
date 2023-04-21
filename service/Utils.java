10ab24def41e9654356caae2219788c64804c0d8
Reinaldo Assis <randroi75@gmail.com>
2023-04-21 18:15:14 -0300

    feat: message dialog boxes

--- service/Utils.java
+++ service/Utils.java
@@ -1,17 +1,24 @@
 package service;
-import java.util.Scanner;
+
+import javax.swing.JOptionPane;
 
 public class Utils {
-    public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+
+    public static void messageBox(String message) {
+        JOptionPane.showMessageDialog(null, message);
     }
 
-    public static void awaitInput() {
-        Scanner leitor = new Scanner(System.in);
-        System.out.println("");
-        System.out.println("Pressione ENTER para continuar...");
-        leitor.nextLine();
+    public static boolean yesNoBox(String message, String title) {
+        int result = JOptionPane.showConfirmDialog(null, message, title, JOptionPane.YES_NO_OPTION);
+        return result == JOptionPane.YES_OPTION;
+    }
+
+    public static void errorBox(String message, String error) {
+        JOptionPane.showMessageDialog(null, message, "Error: "+error, JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void successBox(String message) {
+        JOptionPane.showMessageDialog(null, message, "Success", JOptionPane.INFORMATION_MESSAGE);
     }
 
 }
